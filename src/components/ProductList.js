d5b6529f6b817f8656598ee39c783133712b1aeb
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-07 03:14:43 +0500

    add some comment to the code

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -88,6 +88,11 @@ const ProductList = () => {
     setCart(updatedCart);
   };
 
+  // const numbers = [1, 2, 3, 4, 5];
+
+  // const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  // console.log(sum); // Output: 15
+
   useEffect(() => {
     const cost = cart.reduce(
       (acc, item) => acc + item.price * item.quantity, 0,
